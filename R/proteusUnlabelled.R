b335d6dbfc966255e43462e2c5359ef81a42be6a
Marek Gierlinski <M.Gierlinski@dundee.ac.uk>
2018-02-13 13:28:27 +0000

    Added peptides.txt and proteinGroups.txt

--- R/proteusUnlabelled.R
+++ R/proteusUnlabelled.R
@@ -1,8 +1,8 @@
 #' @title Proteus example unlabelled data
 #' @name proteusUnlabelled
 #' @docType data
-#' @description Example data set for \code{proteus} package. It comes from a label-free MS/MS experiment in two conditions
-#' (called 1112 and BMO-1) and seven replicates each.
+#' @description Example data set for \code{proteus} package. It comes from a label-free MS/MS
+#' experiment in two conditions (called 1112 and BMO-1) and seven replicates each.
 #'
 #' @format Six objects from different stages of data processing:
 #' \describe{
@@ -13,7 +13,8 @@
 #'   \item{\code{pepdat.clean}}{Clean peptide data (\code{proteusData} object)}
 #'   \item{\code{prodat}}{Protein data (\code{proteusData} object)}
 #' }
-#' This package also contains the full original evidence file and formatted metadata file (see examples below).
+#' This package also contains the full original evidence file, peptides file, proteinGroups
+#' file and formatted metadata file (see examples below).
 #'
 #' @examples
 #' data(proteusUnlabelled)
@@ -22,6 +23,8 @@
 #' head(prodat$tab)
 #'
 #' evidenceFile <- system.file("extdata", "evidence.txt.gz", package="proteusUnlabelled")
+#' peptidesFile <- system.file("extdata", "peptides.txt.gz", package="proteusUnlabelled")
+#' proteinGroupsFile <- system.file("extdata", "proteinGroups.txt.gz", package="proteusUnlabelled")
 #' metadataFile <- system.file("extdata", "metadata.txt", package="proteusUnlabelled")
 #"proteusUnlabelled"
 NULL
